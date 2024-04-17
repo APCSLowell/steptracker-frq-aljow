752f152f3b386ea466cdbcda7330b02106b9ed00
aljow <143014720+aljow@users.noreply.github.com>
2024-04-17 10:07:02 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,20 @@
 import java.util.ArrayList;
-public class StepTracker
-{
- /* to be implemented here */
+public class StepTracker { 
+ private int n;
+ public StepTracker(int steps) {
+  n = steps; 
+ }
+ public void addDailySteps() {
+  if (n >= 10000) {
+   System.out.println("This represents an active day.");
+  } else {
+   System.out.println("This is too few steps for the day to be considered active.");
+  }
+ }
+ public int activeDays() {
+
+ }
+ public double averageSteps() {
+
+ }
 } 

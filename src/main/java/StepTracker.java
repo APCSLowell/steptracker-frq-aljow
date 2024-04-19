c0401cb37082b420c89b463173a1f67065bb21f4
aljow <143014720+aljow@users.noreply.github.com>
2024-04-19 11:18:04 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -31,6 +31,6 @@ public class StepTracker {
   for(int i = 0; i < arr.size(); i++) {
    stepAverage += arr.get(i);
   }
+  return stepAverage/arr.size();
  }
- return (double) stepAverage/arr.size();
 } 
